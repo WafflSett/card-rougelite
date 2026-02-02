be5762f6bbfc3010d74158add7b79d7b47bb5632
WafflSett <toros.gergo@students.jedlik.eu>
2026-02-02 12:28:04 +0100

    components created

---
+++ src/app/arena/spell/spell.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-spell',
+  imports: [],
+  templateUrl: './spell.html',
+  styleUrl: './spell.css',
+})
+export class Spell {
+
+}
