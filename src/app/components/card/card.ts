e1aee31a3b9fe59cb03125f9206913b9a2892879
WafflSett <toros.gergo@students.jedlik.eu>
2026-02-04 12:29:55 +0100

    3d effect on cards

--- src/app/components/card/card.ts
+++ src/app/components/card/card.ts
@@ -12,7 +12,7 @@ export class Card {
 
   spell: ISpell | undefined;
   unit: IUnit | undefined;
-  
+
   ngOnInit() {
     console.log(this.card);
     if (this.card.type == 'unit') {
@@ -20,5 +20,25 @@ export class Card {
     } else if (this.card.type == 'spell') {
       this.spell = this.card as ISpell;
     }
+  }
+
+  pointerMove(e:PointerEvent){
+    const x = e.x;
+    const y = e.y;
+    console.log(e.target);
+    const cardElement = (e.target as HTMLDivElement);
+    const rect = cardElement.getBoundingClientRect();
+    const hw = rect.width / 2;
+    const hh = rect.height / 2;
+    const ratioX = (x - (rect.x + hw)) / hw;
+    const ratioY = (y - (rect.y + hh)) / hh;
+    cardElement.style.setProperty("--ratio-x", ratioX.toString());
+    cardElement.style.setProperty("--ratio-y", ratioY.toString());
+  }
+
+  pointerLeave(e:any){
+    const cardElement = (e.target as HTMLDivElement);
+    cardElement.style.setProperty("--ratio-x", "0");
+    cardElement.style.setProperty("--ratio-y", "0");
   }
 }
